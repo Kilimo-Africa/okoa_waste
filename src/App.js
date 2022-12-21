04158461ef2bcc6f9a48eb3b5e1c56242b912f0f
Enock <enock.mokua@student.moringaschool.com>
2022-12-22 01:06:40 +0300

    connect navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Landing from "./pages/landing/landing";
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Landing />
     </div>
   );
