212d3a70e6ed23744907b2e560f5dab2a655b097
Enock <enock.mokua@student.moringaschool.com>
2022-12-22 01:17:03 +0300

    style title

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function Navbar() {
   return (
     <header>
-      <span></span>
-      <span></span>
-      <span></span>
+      <div id="logo">
+        <span id="logo-title">Okoa Organics</span>
+        <span id="logo-phrase">Collect Sort Decompose</span>
+      </div>
+      <div></div>
+      <div></div>
     </header>
   );
 }
