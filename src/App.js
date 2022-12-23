4f8ab507f1e39f799932365f79169e48dafea894
Enock <enock.mokua@student.moringaschool.com>
2022-12-23 14:12:04 +0300

    add browser links to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Landing from "./pages/landing/landing";
 import Navbar from "./pages/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Landing />
+      {/* <Navbar />
+      <Landing /> */}
+
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
