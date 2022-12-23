c7716f49db14c2c3df27302402be0e35e087993b
Enock <enock.mokua@student.moringaschool.com>
2022-12-23 14:01:41 +0300

    create default styles for auth forms

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Signup() {
   return (
-    <main>
+    <main className="form-main">
       <form className="form"></form>
       <div className="form-image"></div>
     </main>
