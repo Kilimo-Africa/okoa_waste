5166801890afff272b9f4b08f743d91688996764
Enock <enock.mokua@student.moringaschool.com>
2022-12-23 13:49:28 +0300

    TITLE TO LOGO

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <header>
       <div id="logo">
         <span id="logo-title">Okoa Organics</span>
-        <span id="logo-phrase">Collect Sort Decompose</span>
+        <span id="logo-phrase">COLLECT SORT DECOMPOSE</span>
       </div>
       <div></div>
       <div></div>
