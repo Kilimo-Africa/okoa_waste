3f94c4c2c17736cf19758b492922d319ad424100
Enock <enock.mokua@student.moringaschool.com>
2022-12-23 14:14:26 +0300

    connect login to app paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from "./pages/landing/landing";
 import Navbar from "./pages/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/auth/signup";
+import Login from "./pages/auth/login";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
