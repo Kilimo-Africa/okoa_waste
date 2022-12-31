5980ef88ebb2091d879796b0b151e146b5c7ce04
Enock <enock.mokua@student.moringaschool.com>
2023-01-01 02:18:03 +0300

    add signup form sections

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -3,7 +3,10 @@ import "./auth.css";
 export default function Signup() {
   return (
     <main className="form-main">
-      <div className="form-image"></div>
+      <div className="form-image">
+        <section id="form-section1"></section>
+        <section id="form-section2"></section>
+      </div>
       <form className="form"></form>
     </main>
   );
