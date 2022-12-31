69b5c80ef93d18ccb4a8e4d9d0c758379d32bfe7
Enock <enock.mokua@student.moringaschool.com>
2023-01-01 02:16:22 +0300

    format format form image and form for signup

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -3,6 +3,7 @@ import "./auth.css";
 export default function Signup() {
   return (
     <main className="form-main">
+      <div className="form-image"></div>
       <form className="form"></form>
     </main>
   );
