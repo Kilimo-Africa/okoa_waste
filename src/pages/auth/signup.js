51f7641358b695e02f57f21e3e3c71d92acfe0bd
Enock <enock.mokua@student.moringaschool.com>
2023-01-01 02:11:19 +0300

    restructure signup form

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -4,10 +4,6 @@ export default function Signup() {
   return (
     <main className="form-main">
       <form className="form"></form>
-      <div className="form-image">
-        <span id="form-image1"></span>
-        <span id="form-image2"></span>
-      </div>
     </main>
   );
 }
