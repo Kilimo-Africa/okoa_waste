2d5fb6e2139894ea8b33b31f1783a51430372b16
Enock <enock.mokua@student.moringaschool.com>
2023-01-01 02:57:00 +0300

    create sections for form section1

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -6,7 +6,10 @@ export default function Signup() {
       <div className="form-image"></div>
       <form className="form">
         <span>Join Okoa</span>
-        <section id="form-section1"></section>
+        <section id="form-section1">
+          <div id="form-section1-container"></div>
+          <div id="form-section1-links"></div>
+        </section>
         <section id="form-section2"></section>
       </form>
     </main>
