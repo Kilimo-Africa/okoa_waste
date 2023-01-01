51e0433106d688637965f468d8a7523840b3870f
Enock <enock.mokua@student.moringaschool.com>
2023-01-01 03:02:12 +0300

    add next button and style it

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./auth.css";
 export default function Signup() {
   return (
@@ -8,7 +9,10 @@ export default function Signup() {
         <span>Join Okoa</span>
         <section id="form-section1">
           <div id="form-section1-container"></div>
-          <div id="form-section1-links"></div>
+          <div id="form-section1-links">
+            
+            <Link className="links">Next</Link>
+          </div>
         </section>
         <section id="form-section2"></section>
       </form>
