7c5f3ed161dcd1d05076f2bc40a343d28ceb28bf
Enock <enock.mokua@student.moringaschool.com>
2023-01-01 03:20:48 +0300

    add labels and style them

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -8,12 +8,27 @@ export default function Signup() {
       <form className="form">
         <span>Join Okoa</span>
         <section id="form-section1">
-          <div id="form-section1-container"></div>
+          <div id="form-section1-container">
+            <label>
+              Disposer
+              <input type="radio" />
+            </label>
+
+            <label>
+              Farmer
+              <input type="radio" />
+            </label>
+
+            <label>
+              Partner
+              <input type="radio" />
+            </label>
+          </div>
           <div id="form-section1-links">
-            
             <Link className="links">Next</Link>
           </div>
         </section>
+
         <section id="form-section2"></section>
       </form>
     </main>
