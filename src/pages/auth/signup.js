79e081714cb5c553a1d8b85f9aa3b41e6959a730
Enock <enock.mokua@student.moringaschool.com>
2023-01-02 14:55:18 +0300

    change background color of select tags and option

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -46,7 +46,7 @@ export default function Signup() {
           </div>
 
           <span id="roles-divs">
-            <span id="role-desc">How would you like to use Okoa</span>
+            <span id="role-desc">How would you like to use Okoa?</span>
             <select id="roles">
               <option value="2">Dispose waste and earn from it.</option>
               <option value="3">Buy compost and sell your produce.</option>
