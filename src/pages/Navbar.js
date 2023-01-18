17bc1a20c39425658fd15f9c067378d87f088dda
Enock <enock.mokua@student.moringaschool.com>
2023-01-19 01:56:44 +0300

    add links to the navigation bar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header>
-      <div id="logo">
+    <nav>
+      <Link id="logo" to="/">
         <span id="logo-title">Okoa Organics</span>
         <span id="logo-phrase">COLLECT SORT DECOMPOSE</span>
+      </Link>
+      <div>
+        <Link to="/services">Services</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
       </div>
       <div></div>
-      <div></div>
-    </header>
+    </nav>
   );
 }
