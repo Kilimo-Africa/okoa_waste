4353464ef028ca558370305414ae6b5218cbd4b0
Enock <enock.mokua@student.moringaschool.com>
2023-01-19 01:40:21 +0300

    remove default styling for main page

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -4,92 +4,29 @@ import { useState } from "react";
 import "./auth.css";
 
 export default function Signup() {
-  let [role, setRole] = useState("");
-  let [email, setEmail] = useState("");
-  let [password, setPassword] = useState("");
-  let [password_c, setPasswordC] = useState("");
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   let user = {
+  //     role_id: role,
+  //     email,
+  //     password,
+  //     password_confirmation: password_c,
+  //   };
 
-  function handleSelect(event) {
-    setRole(event.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    let user = {
-      role_id: role,
-      email,
-      password,
-      password_confirmation: password_c,
-    };
-
-    fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-
-  }
+  //   fetch("http://localhost:3000/users", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(user),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }
 
   return (
     <main className="form-main">
       <div className="form-image"></div>
-      <form className="form" onSubmit={handleSubmit}>
-        <span>Join Okoa</span>
-
-        <section id="form-section2">
-          <div id="device-auth">
-            <span>Facebook</span>
-            <span>Google</span>
-          </div>
-
-          <span id="roles-divs">
-            {/* <span id="role-desc">How would you like to use Okoa?</span> */}
-            <select id="roles" onChange={handleSelect}>
-              <option>Select how you would like to use Okoa.</option>
-              <option value="2">Dispose waste and earn from it.</option>
-              <option value="3">Buy compost and sell your produce.</option>
-              <option value="4">Partner with us</option>
-            </select>
-          </span>
-
-          <div id="signup-email">
-            <input
-              type="email"
-              placeholder="E-Mail address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div id="signup-names">
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={password_c}
-              onChange={(e) => setPasswordC(e.target.value)}
-            />
-          </div>
-
-          <div id="signup-button">
-            <button type="submit">Join Okoa</button>
-          </div>
-
-          <div id="signup-links">
-            <Link to="">Back</Link>
-            <Link to="">Login</Link>
-          </div>
-        </section>
-      </form>
     </main>
   );
 }
