07bddad9d5fb435a979a08fbc73990b4d5a62285
Enock <enock.mokua@student.moringaschool.com>
2023-01-27 12:38:25 +0300

    style footer section 2

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+     <div className="footer-1"></div>
+     <div className="footer-2">
+      <p>From the wonderful people at Okoa Tech</p>
+     </div>
     </footer>
   );
 }
