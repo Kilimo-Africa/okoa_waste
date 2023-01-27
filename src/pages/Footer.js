6e97c7ee7574341222d9e5e7257a971eb84af5f5
Enock <enock.mokua@student.moringaschool.com>
2023-01-27 12:47:08 +0300

    create basic structure of footer 1 and add styling

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-     <div className="footer-1"></div>
+     <div className="footer-1">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+     </div>
      <div className="footer-2">
       <p>From the wonderful people at Okoa Tech</p>
      </div>
