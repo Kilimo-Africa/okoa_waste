3da99003e291f1b75c0a1e5348d7ff1889edca8e
Enock <enock.mokua@student.moringaschool.com>
2023-02-08 12:26:58 +0300

    Create email an password inputs for email signup

--- src/pages/auth/email.js
+++ src/pages/auth/email.js
@@ -1,9 +1,28 @@
+import "./auth.css";
+
 export default function SignupEmail() {
   return (
     <main className="form-main">
       <div className="form-image"></div>
       <div className="form-container">
-
+        <input
+          type="text"
+          placeholder="Enter your E-mail"
+          className="email-inputs"
+        />
+        <input
+          type="text"
+          placeholder="Enter your Password"
+          className="email-inputs"
+        />
+        <input
+          type="text"
+          placeholder="Confirm your password"
+          className="email-inputs"
+        />
+        <button type="submit" className="email-input-submit">
+            Join Okoa 
+        </button>
       </div>
     </main>
   );
