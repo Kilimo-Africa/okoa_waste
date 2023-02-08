a05d8a5ed2c8c58c7605a36ca26bf17e6c56ea50
Enock <enock.mokua@student.moringaschool.com>
2023-02-08 12:20:57 +0300

    create default dtructure of signup with email

--- src/pages/auth/email.js
+++ src/pages/auth/email.js
@@ -1,3 +1,10 @@
-export default function SignupEmail(){
-    return<></>
+export default function SignupEmail() {
+  return (
+    <main className="form-main">
+      <div className="form-image"></div>
+      <div className="form-container">
+
+      </div>
+    </main>
+  );
 }
